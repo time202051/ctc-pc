b905c9cd427c1b0176d9c810ef4cf53401a5d74b
jiapeng <59959780+time202051@users.noreply.github.com>
2025-10-21 15:57:15 +0800

    Add files via upload

---
+++ layout/components/AppMain.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="app-main">
+    <!--    <transition name="fade-transform" mode="out-in">-->
+    <keep-alive :include="cachedViews">
+      <router-view :key="key" />
+    </keep-alive>
+    <!--    </transition>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppMain",
+  computed: {
+    cachedViews() {
+      return this.$store.state.tagsView.cachedViews;
+    },
+    key() {
+      return this.$route.path;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.app-main {
+  /* 50= navbar  50  */
+  // min-height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+  overflow: auto;
+  > div:first-child {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+}
+
+.fixed-header + .app-main {
+  padding-top: 50px;
+}
+
+.hasTagsView {
+  .app-main {
+    /* 84 = navbar + tags-view = 50 + 34 */
+    min-height: calc(100vh - 84px);
+    height: calc(100vh - 84px);
+  }
+
+  .fixed-header + .app-main {
+    padding-top: 84px;
+  }
+}
+</style>
+
+<style lang="scss">
+// fix css style bug in open el-dialog
+.el-popup-parent--hidden {
+  .fixed-header {
+    padding-right: 15px;
+  }
+}
+</style>
