c2d06acbd39466f3657b32d4143ea78334d74ff8
jiapeng <59959780+time202051@users.noreply.github.com>
2025-10-21 15:19:23 +0800

    Add files via upload

---
+++ public/configBaseUrl.js
@@ -0,0 +1,9 @@
+window.global_config = {
+  // baseURL: "http://172.16.1.11:8080/ctc",
+  baseURL: "http://220.179.249.140:20019",
+  // baseURL: "http://192.168.101.226:20019",
+  // baseURL: "http://220.179.249.140:20019",
+  // baseURL: "http://220.179.249.140:20022",
+  // baseURL: "http://192.168.101.242:8081",//晋力
+  swaggerUrl: "/ctc" // "/ctc"
+};
